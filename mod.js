abddef942c75f18d1959f9b0b07b4d682c394295
Leo Esc <leonardodanielescoto@gmail.com>
2022-03-28 21:27:14 -0500

    Add files via upload

---
+++ mod.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "AI Upscaled AIDS", 
+    summary: "AIDS upscaled with Waifu2X",
+    author: "Sharkalien",
+    modVersion: 1,
+
+    description: "The pages of And It Don't Stop upscaled with Waifu2X to be twice as large",
+
+    trees: {
+        "./AIDS_AI_upscaled/": "assets://archive/comics/tso/aids/"
+    },
+	
+	styles: [
+    {
+        source: "./theme.scss"
+    }
+	]
+}
