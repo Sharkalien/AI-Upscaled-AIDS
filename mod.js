1cb92bdcb1e3fd19a66c5657d2eb33d866e74e7e
Leo Esc <leonardodanielescoto@gmail.com>
2022-04-02 05:45:48 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -2,7 +2,7 @@ module.exports = {
     title: "AI Upscaled AIDS", 
     summary: "AIDS upscaled with Waifu2X",
     author: "Sharkalien",
-    modVersion: 1,
+    modVersion: 1.01,
 
     description: "The pages of And It Don't Stop upscaled with Waifu2X to be twice as large",
 
